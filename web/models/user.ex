0af2f213b337804d7e740304f4c6aaebeb563994
Nathan Verni <npverni@gmail.com>
2016-09-05 23:55:32 -0400

    Generators and Relationships

--- web/models/user.ex
+++ web/models/user.ex
@@ -1,11 +1,3 @@
-#---
-# Excerpted from "Programming Phoenix",
-# published by The Pragmatic Bookshelf.
-# Copyrights apply to this code. It may not be used to create training material,
-# courses, books, articles, and the like. Contact us if you are in doubt.
-# We make no guarantees that this code is fit for any purpose.
-# Visit http://www.pragmaticprogrammer.com/titles/phoenix for more book information.
-#---
 defmodule Rumbl.User do
   use Rumbl.Web, :model
 
@@ -14,6 +6,7 @@ defmodule Rumbl.User do
     field :username, :string
     field :password, :string, virtual: true
     field :password_hash, :string
+    has_many :videos, Rumbl.Video
 
     timestamps
   end
