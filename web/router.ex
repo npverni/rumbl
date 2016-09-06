694fc21ab0e5660286ff31bff4f59bf780edbc6c
Nathan Verni <npverni@gmail.com>
2016-09-05 23:21:49 -0400

    Generate Videos

--- web/router.ex
+++ web/router.ex
@@ -20,5 +20,6 @@ defmodule Rumbl.Router do
     get "/", PageController, :index
     resources "/users", UserController, only: [:index, :show, :new, :create]
     resources "/sessions", SessionController, only: [:new, :create, :delete]
+    resources "/videos", VideoController
   end
 end
